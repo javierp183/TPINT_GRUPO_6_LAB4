aea2a6f6f792d390e9fdae1cbfa34957164a60d9
LordJuani <juanignacio.corrionero@gmail.com>
2020-10-29 22:42:46 -0300

    Agregado login falta boostrap

--- src/dao/ClienteDao.java
+++ src/dao/ClienteDao.java
@@ -8,5 +8,5 @@ public interface ClienteDao {
 	public boolean insert(Cliente cliente);
 	public boolean delete(Cliente cliente_a_eliminar);
 	public boolean modify(Cliente c);
-	//public List<Cliente> readAll();
+	public List<Cliente> readAll();
 }
