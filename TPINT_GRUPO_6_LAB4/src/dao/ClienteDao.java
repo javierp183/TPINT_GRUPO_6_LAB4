e0f85e75ec4996a1c17ea36eefd1ffde147dc425
Javier Pereyra <javier.pereyra@gmail.com>
2020-10-30 00:43:21 -0300

    mas vistas y html

---
+++ TPINT_GRUPO_6_LAB4/src/dao/ClienteDao.java
@@ -0,0 +1,12 @@
+package dao;
+
+import java.util.List;
+
+import dominio.Cliente;
+
+public interface ClienteDao {
+	public boolean insert(Cliente cliente);
+	public boolean delete(Cliente cliente_a_eliminar);
+	public boolean modify(Cliente c);
+	//public List<Cliente> readAll();
+}
